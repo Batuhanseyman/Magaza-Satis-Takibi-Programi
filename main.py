b1f5e6bbfa99fbb09c911c94435d3acbf1628623
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2023-05-18 14:55:56 +0300

    Attribute'lar için mutator metodları yazıldı.

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ class Magaza :
         self.__magaza_adi = magaza_adi
         self.__satici_adi = satici_adi
         self.__satici_cinsi = satici_cinsi
-        
+
+    def set_magaza_adi (self, magaza_adi):
+        self.__magaza_adi = magaza_adi
+
+    def set_satici_adi (self, satici_adi):
+        self.__satici_adi = satici_adi
+
+    def set_satici_cinsi (self, satici_cinsi):
+        self.__satici_cinsi = satici_cinsi
  
                             
 def main ():
